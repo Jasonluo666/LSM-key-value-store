51d3c1b76a29ffffa2ab023919582e6371b06dc1
Jasonluo996 <jasonluo996@gmail.com>
2019-04-27 19:46:41 -0400

    sort func done

--- LSM_demo/LSM.hpp
+++ LSM_demo/LSM.hpp
@@ -10,15 +10,50 @@ template<typename K, typename V>
 class LSM {
 private:
     typedef Pair<K,V> KV_pair;
-    Buffer<K,V> buff;
+    Buffer<K,V>* buff;
     DiskRun<K,V>* runs;
     int buffer_size;
     int max_level;
     int runs_per_level;
     int page_size;
-public:
 
+	// sort two sorted arrays
+	vector<KV_pair> trickySort(vector<KV_pair> new_vec, vector<KV_pair> old_vec) {
+		int pointer1 = 0, pointer2 = 0;
+		vector<KV_pair> merged;
+
+		// two pointers scan thru the vectors
+		while (pointer1 < new_vec.size() && pointer2 < old_vec.size()) {
+			if (new_vec[pointer1] < old_vec[pointer2]) {
+				merged.push_back(new_vec[pointer1]);
+				pointer1 += 1;
+			}
+			else if (new_vec[pointer1] > old_vec[pointer2]) {
+				merged.push_back(old_vec[pointer2]);
+				pointer2 += 1;
+			}
+			else {	// equal -> check if it's TOMBSTONE
+				if (new_vec[pointer1].value != TOMBSTONE)
+					merged.push_back(new_vec[pointer1]);
+
+				pointer1 += 1;
+				pointer2 += 1;
+			}
+		}
+
+		// append the leftover
+		if (pointer1 < new_vec.size())
+			merged.insert(merged.end(), new_vec.begin() + pointer1, new_vec.end());
+		// append the leftover
+		if (pointer2 < old_vec.size())
+			merged.insert(merged.end(), old_vec.begin() + pointer2, old_vec.end());
+
+		return merged;
+	}
+
+public:
     LSM(){
+
     }
 
     LSM(int buffer_size, int page_size, int max_level, int runs_per_level){
@@ -26,7 +61,7 @@ public:
         this->page_size = page_size;
         this->max_level = max_level;
         this->runs_per_level = runs_per_level;
-        buff.Buffer(buffer_size/sizeof(KV_pair));
+        buff = new Buffer<K, V>(buffer_size/sizeof(KV_pair));
         runs = new DiskRun<K,V>[max_level];
     }
 
@@ -40,6 +75,7 @@ public:
             }
 	    }
 	}
+
 	vector<Pair<K, V> > range(K key_min, K key_max) {
 
 	}
--- LSM_demo/main.cpp
+++ LSM_demo/main.cpp
@@ -1,5 +1,7 @@
 #include "iostream"
 #include "DiskRun.hpp"
+#include "BloomFilter.hpp"
+#include "LSM.hpp"
 
 using namespace std;
 
@@ -15,6 +17,32 @@ int main(int argc, char* argv) {
 	
 	diskRun.merge(test);
 	diskRun.rangeSearch(0, 10);
+
+	int test_num = 10000;
+	BloomFilter<int> filter(test_num, 0.1);
+	for(int i = 0; i < test_num; i++) {
+		filter.addKey(i);
+	}
+
+	int matrix[2][2] = {0, 0, 0, 0};
+	for(int i = 0; i < test_num * 2; i++) {
+
+		if (filter.contain(i))
+			if (i < test_num)
+				matrix[0][0] += 1;
+			else
+				matrix[0][1] += 1;
+		else
+			if (i < test_num)
+				matrix[1][0] += 1;
+			else
+				matrix[1][1] += 1;
+	}
+
+	cout << "TP" << matrix[0][0] << endl;
+	cout << "FP" << matrix[0][1] << endl;
+	cout << "FN" << matrix[1][0] << endl;
+	cout << "TN" << matrix[1][1] << endl;
 
 	system("pause");
 	return 0;
